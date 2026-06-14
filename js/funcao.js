07b73bebbf2cf54c1a3ec89f2548a3aa7ba662be
Dora <trllyiza@gmail.com>
2026-06-14 15:27:21 -0300

    agenda2

--- js/funcao.js
+++ js/funcao.js
@@ -70,6 +70,7 @@ async function fazerLogin(){
 }
 
 //TELA AGENDA
+
 const modalTarefa = document.getElementById('modalTarefa');
 const adicionarTarefa = document.getElementById('adicionarTarefa');
 const criarTarefa = document.getElementById('criarTarefa');
@@ -85,7 +86,7 @@ adicionarTarefa.addEventListener('click', () => {
     modalTarefa.style.display = 'block';
 });
 }
-
+//Fecha a sobreposição ao enviar tarefa
 if (criarTarefa) {
 criarTarefa.addEventListener('click', async (event) => {
 
@@ -112,11 +113,11 @@ criarTarefa.addEventListener('click', async (event) => {
         }
 
     modalTarefa.style.display = 'none';
-    await enviarTarefa(tarefa);
+    await enviarTarefa(tarefa); //Deve enviar a tarefa para o banco 
     document.getElementById('formTarefa').reset();
 });
 }
-
+//Enviando a tarefa para o banco
 async function enviarTarefa(tarefa){
     try {
         const resposta = await fetch(URL_API_TAREFA, {
@@ -135,3 +136,58 @@ async function enviarTarefa(tarefa){
         alert('Não foi possível conectar ao servidor.');
     }
 }
+
+//Pegar as tarefas do banco
+async function mostrarTarefas(tarefa) {
+    try{
+        const resposta = await fetch(URL_API_TAREFA);
+        const dados = await resposta.json();
+
+        const tempoLimite = dataEntrega - dataAtual;
+
+        document.getElementById('semana1').innerHTML = '';
+        document.getElementById('semana2').innerHTML = '';
+        document.getElementById('semana3').innerHTML = '';
+        document.getElementById('semana4').innerHTML = '';
+
+        dados.forEach(item => {
+
+            // Atribuindo os valores do objeto 'item' para as suas variáveis
+            const disciplinaValor = item.disciplina;
+            const descricaoValor = item.descricao;
+            const dataAtualValor = new Date(item.dataAtual);
+            const dataEntregaValor = new Date(item.dataEntrega);
+
+            const diferencaEmMilissegundos = dataEntregaValor - dataAtualValor; 
+            const diferencaEmDias = Math.floor(diferencaEmMilissegundos / (1000 * 60 * 60 * 24));
+            const tempoLimite = diferencaEmDias + " dias";
+
+            let semanaTarefa = "";
+            if(diferencaEmDias >= 0 && diferencaEmDias < 7){
+                let semanaTarefa = "semana1"
+            }
+            else if(diferencaEmDias < 14){
+                let semanaTarefa = "semana2"
+            }
+            else if(diferencaEmDias < 21){
+                let semanaTarefa = "semana3"
+            }
+            else if(diferencaEmDias < 28){
+                let semanaTarefa = "semana4"
+            }
+
+                const listaTarefas = document.getElementById(semanaTarefa);
+                if(listaTarefas){
+                     listaTarefas.innerHTML += `
+                     <li>${disciplinaValor}</li>
+                     <li>${descricaoValor}</li>
+                     <li>${tempoLimite}</li>`;
+                }
+        });
+
+        } catch (erro) {
+        console.error('Erro ao buscar o dado:', erro);
+  }
+}
+
+        
